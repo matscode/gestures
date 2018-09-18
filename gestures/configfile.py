1976e311786428a0e08c8f4447c6fee35fda57c6
Unknown <raffaele.tranquillini@gmail.com>
2018-09-18 12:49:53 +0200

    .conf parsing bugfix
    converts tabs to spaces instead of ignoring

--- gestures/configfile.py
+++ gestures/configfile.py
@@ -48,6 +48,9 @@ class ConfigFileHandler:
         for line in self.rawLines:
             validLine = self.validSupportedRegex.match(line)
             if(validLine):
+                print(line)
+                line = line.replace('\t', ' ')
+                print(line)
                 line = line.split(' ')
                 filter(None, line)
 
